3a22ea275ba25d7879d2dd96c2b8b6c295b7df79
ridwanuwu <64183537+ridwanuwu@users.noreply.github.com>
2021-11-24 09:18:51 +0700

    Initial commit

---
+++ lib/model/user.dart
@@ -0,0 +1,6 @@
+class User {
+  String nama;
+  String email;
+  String password;
+  User({required this.nama, required this.email, required this.password});
+}
---
+++ lib/pages/presslogin.dart
@@ -0,0 +1,43 @@
+import 'package:project_uts/model/user.dart';
+import 'genre.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PressLogin extends StatelessWidget{
+  final User user;
+  PressLogin({required this.user});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+            color: Colors.black
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(user.nama),
+            Text(user.email),
+            Text(user.password),
+            FloatingActionButton(
+                child: Icon(Icons.arrow_forward_sharp),
+                backgroundColor: Colors.purple,
+                elevation: 0,
+                onPressed: (){
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context)=>genrePage())
+                  );
+                }
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
